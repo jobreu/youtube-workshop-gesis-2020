82acc63ac4c4f141210a62c66f72846414ff2880
Julian Kohne <JuKo007@users.noreply.github.com>
2020-02-06 18:23:49 +0100

    Delete RvestVideoDescription.R

--- NotPublic/RvestVideoDescription.R
+++
@@ -1,7 +0,0 @@
-library(rvest)
-library(magrittr)
-url <- "https://www.youtube.com/watch?v=4PqdqWWSHJY"
-url %>%
-  read_html %>%
-  html_nodes(xpath = "//*[@id = 'eow-description']") %>%
-  html_text
